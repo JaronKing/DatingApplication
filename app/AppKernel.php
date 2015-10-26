23f9309d9a6feb749c879c779cb35a879d17e5c3
Jaron <jaronjking@gmail.com>
2015-10-26 09:55:50 -0700

    Added Fos:User:bundle and extended User class to DatingUserBundle.  Configured routing but requires templates to be configured.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,8 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new Dating\MainBundle\DatingMainBundle(),
             new Dating\AdminBundle\DatingAdminBundle(),
+            new Dating\UserBundle\DatingUserBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
